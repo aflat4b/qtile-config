6f5fd0b01fa7c5ac1ff1158e06cfa711cf598beb
Andrea Biondini <abiondini73@gmail.com>
2025-11-19 10:40:12 +0100

    Added startup hook

--- config.py
+++ config.py
@@ -4,9 +4,8 @@
 # / ___ \|  _| | (_| | |_ 
 #/_/   \_\_| |_|\__,_|\__|
 
-import os
-
-from libqtile import layout
+import os, subprocess
+from libqtile import layout, hook
 from keys import keys, mouse
 from groups import groups
 from layouts import layouts
@@ -18,6 +17,14 @@ from colorschemes.catppuccin.catppuccin_macchiato import colors
 
 # This is the main entry of my qtile config
 
+# Startup hook
+
+@hook.subscribe.startup_once
+def autostart():
+    script = os.path.expandusers("~/.config/autostart/autostart.sh")
+    subprocess.run(script)
+
+# Qtile variables
 dgroups_key_binder = None
 follow_mouse_focus = True
 bring_front_click = False
@@ -27,5 +34,5 @@ floating_layout = layout.Floating(border_focus = colors[17][0],
                                   border_width = 2,
                                   fullscreen_border_width = 2)
 focus_on_window_activation = 'smart'
-wmname = 'qtile'
+wmname = 'LG3D'
                          
